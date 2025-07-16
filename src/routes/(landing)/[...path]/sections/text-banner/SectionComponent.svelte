27f5ef49ce43919337d4d81fae3cf1f3484ec310
sallliisa <sodiumcation@gmail.com>
2025-07-16 13:56:38 +0700

    gamer

--- src/routes/(landing)/[...path]/sections/text-banner/SectionComponent.svelte
+++ src/routes/(landing)/[...path]/sections/text-banner/SectionComponent.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
-  import SectionHeader from '$lib/app/components/app/SectionHeader.svelte';
   import { m } from '$lib/paraglide/messages';
-  import { onMount, onDestroy } from 'svelte';
 
   const {section} = $props()
 </script>
 
 <div class="flex items-center justify-center w-full">
-  <div class="w-full max-w-screen-xl py-6 lg:py-12 px-6 lg:px-12 grid grid-cols-3 gap-lg">
+  <div class="w-full max-w-screen-xl py-6 lg:py-12 px-6 lg:px-12 grid md:grid-cols-2 grid-cols-1 gap-lg">
     <div class="col-span-1">
-      <img src="{section.data.content.media}" alt="{section.data.content.title}" class="w-full object-center object-cover outline outline-outline-variant"/>
+      <img src="{section.data.content.media}" alt="{section.data.content.title}" class="w-full object-center object-cover"/>
     </div>
-    <div class="col-span-2 flex flex-col gap-sm">
+    <div class="col-span-1 flex flex-col gap-sm">
       <div class="flex flex-col gap-xs">
         {#if section.data.content.subtitle}<p class="text-sm">{section.data.content.subtitle}</p>{/if}
         {#if section.data.content.title}<p class="text-2xl md:text-3xl font-bold ">{section.data.content.title}</p>{/if}
