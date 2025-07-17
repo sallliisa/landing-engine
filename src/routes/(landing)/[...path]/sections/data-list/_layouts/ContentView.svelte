7be2d9d0847c4a30c65e3bcb72433a9c9163891b
gamer <sodiumcation@gmail.com>
2025-07-17 15:52:16 +0700

    gamer

--- src/routes/(landing)/[...path]/sections/data-list/_layouts/ContentView.svelte
+++ src/routes/(landing)/[...path]/sections/data-list/_layouts/ContentView.svelte
@@ -11,11 +11,12 @@
     {#each childSection as child (child.id || child.name)}
       <Accordion.Item value={child.id || child.name} class="flex flex-col py-6 border-b border-outline-variant">
         <Accordion.Header>
-          <Accordion.Trigger class="w-full flex flex-row items-center justify-between gap-sm">
+          <Accordion.Trigger
+            class="w-full flex flex-row items-center justify-between gap-sm [&[data-state=open]>.ri-add-line]:hidden [&[data-state=open]>.ri-subtract-line]:block [&[data-state=closed]>.ri-add-line]:block [&[data-state=closed]>.ri-subtract-line]:hidden"
+          >
             <p class="font-semibold min-w-max">{child.title}</p>
-            <div class="data-[state=open]:rotate-90 transition-all">
-              <i class="ri-add-line text-outline"></i>
-            </div>
+            <i class="ri-add-line text-outline"></i>
+            <i class="ri-subtract-line text-outline"></i>
           </Accordion.Trigger>
         </Accordion.Header>
         <Accordion.Content class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden">
