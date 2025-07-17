d6d56a46ba30a9cd1a05f60c48c95d04d61e74f7
sallliisa <sodiumcation@gmail.com>
2025-07-17 22:53:44 +0700

    gamer

--- src/routes/(landing)/[...path]/sections/article-highlights/SectionComponent.svelte
+++ src/routes/(landing)/[...path]/sections/article-highlights/SectionComponent.svelte
@@ -20,7 +20,7 @@
   >
     <div class="w-full max-w-screen-xl flex flex-col py-6 lg:py-12 gap-6 px-6 lg:px-12">
       <SectionHeader header={section.data.content}/>
-      <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-lg">
+      <!-- <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-lg">
         {#if section.data.article && section.data.article.length > 0}
         <a href={`/article/${section.data.article[0].slug}`} class="col-span-1 md:col-span-1 lg:col-span-6 flex flex-col gap-base group">
             <img src="{section.data.article[0].thumbnail}" class="w-full h-[240px] sm:h-[320px] object-center object-cover outline outline-outline-variant" alt={section.data.article[0].title}/>
@@ -45,7 +45,19 @@
         {:else}
           <p class="col-span-full text-start text-outline">{m.no_articles_to_display()}</p>
         {/if}
-      </div> 
+      </div>  -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-base w-full">
+        {#each section.data.article as article (article.id)}
+          <ArticleItem
+            title={article.title}
+            excerpt={article.excerpt}
+            created_at={article.created_at}
+            categories={article.categories}
+            thumbnail={article.thumbnail}
+            slug={article.slug}
+          />
+        {/each}
+      </div>
     </div>
   </div>
 {/if}
