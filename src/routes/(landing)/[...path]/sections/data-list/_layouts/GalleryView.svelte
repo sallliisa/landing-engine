af373202bf757409fcfaf4759d00b9c05460d668
sallliisa <sodiumcation@gmail.com>
2025-07-18 23:35:32 +0700

    gamer

--- src/routes/(landing)/[...path]/sections/data-list/_layouts/GalleryView.svelte
+++ src/routes/(landing)/[...path]/sections/data-list/_layouts/GalleryView.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="relative group aspect-square max-w-[320px]">
-  <ImagePreview src={content.media} title={content.title} description={content.description} class="h-full w-full">
+  <ImagePreview src={content.media} title={content.title} description={content.description} subtitle={content.subtitle} class="h-full w-full">
     {#snippet trigger()}
       <div class="aspect-square w-[320px] text-white outline outline-outline-variant bg-center h-full bg-cover flex flex-col text-start items-start group/item justify-end p-6" style="background-image: {(content.title || content.description) ? `linear-gradient(to top, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 75%), ` : ``} url('{content.media}');">
         <p class="font-semibold translate-y-5 group-hover/item:translate-y-0 transition-all">{content.title}</p>
